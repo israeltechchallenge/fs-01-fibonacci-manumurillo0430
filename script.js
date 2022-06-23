5ba3bd11d623ecb72e50482958f1abc0e82aaade
manumurillo0430 <manumurillo33@gmail.com>
2022-06-23 20:01:06 +0300

    Milestone 5

--- script.js
+++ script.js
@@ -3,13 +3,51 @@ const urlFibonacci = "http://localhost:5050/fibonacci/";
 
 
 function fibonacciServer() {
-    let result;
-    let userNum = document.getElementById('number').value;    
-    fetch(`http://localhost:5050/fibonacci/${userNum}`)
-    .then(response => response.json())  
-    .then(data => {
-        document.getElementById('result').innerText = data.result;
-     });
+    let result = document.getElementById('result')
+    let userNum = document.getElementById('number').value; 
+    let spinner = document.getElementById('spinner'); 
+    let largerThan = document.getElementById('larger-than'); 
+
+    if (userNum > 50) {
+        largerThan.classList.remove('d-none');
+        document.getElementById('number').style.color = "#D9534F";
+        document.getElementById('number').style.borderColor = "#D9534F"
+        spinner.classList.remove('spinner-border');
+        result.classList.add('d-none');
+    } else {
+        result.classList.remove('d-none');
+        spinner.classList.add('spinner-border');
+        document.getElementById('number').style.color = "#373A3C"
+        document.getElementById('number').style.borderColor = "#CCCCCC"
+        largerThan.classList.add('d-none')
+        spinner.classList.remove('d-none');
+        document.getElementById('result').innerText = '';
+        fetch(`http://localhost:5050/fibonacci/${userNum}`)
+        .then(response => {
+            if(!response.ok){
+                return response.text()
+                    .then(errorText => { throw new Error(errorText) })
+            }
+            return response.json()
+        })  
+        .then(data => {
+            document.getElementById('result').innerText = data.result;
+            document.getElementById('result').style.textDecoration = "underline";
+            document.getElementById('result').style.fontWeight = "600";
+            document.getElementById('result').style.color = "black";
+            spinner.classList.add('d-none')
+        })
+        .catch(error => {
+            spinner.classList.add('d-none');
+            result.classList.remove('d-none');
+            document.getElementById('result').innerText = 'Server Error: ' + error.message;
+            document.getElementById('result').style.textDecoration = "none";
+            document.getElementById('result').style.fontWeight = "400";
+            document.getElementById('result').style.color = "#D9534F";
+            document.getElementById('number').style.borderColor = "#CCCCCC"
+        });
+    }
+
 }
 
 document.getElementById('btn').addEventListener('click',fibonacciServer);
