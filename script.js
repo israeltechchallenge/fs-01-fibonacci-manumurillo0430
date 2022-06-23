8acda14bfa5972dd8dfbcee913af42dea89266b2
manumurillo0430 <manumurillo33@gmail.com>
2022-06-23 15:41:26 +0300

    milestone 4

---
+++ script.js
@@ -0,0 +1,16 @@
+const urlFibonacci = "http://localhost:5050/fibonacci/";
+
+
+
+function fibonacciServer() {
+    let result;
+    let userNum = document.getElementById('number').value;    
+    fetch(`http://localhost:5050/fibonacci/${userNum}`)
+    .then(response => response.json())  
+    .then(data => {
+        document.getElementById('result').innerText = data.result;
+
+    });
+}
+
+document.getElementById('btn').addEventListener('click',fibonacciServer);
